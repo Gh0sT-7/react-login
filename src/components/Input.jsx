8318db770465f46cf8936ec5a2aed3c8dde0fa25
James <jamie_c12@hotmail.com>
2024-03-01 14:01:30 +0000

    Added in conditional switching to clean things up.

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,19 @@
-export default function Input({label, invalid, ...props}) {
-    return <p>
-        <label className="block mb-2 text-xs font-bold tracking-wide uppercase text-stone-200">{label}</label>
-        <Input className="shadow border rounded w-full px-3 py-2 bg-stone-300 leading-tight text-gray-700" {...props} />
-    </p>
-};
+export default function CustomInput({ label, invalid, ...props }) {
+    let labelClasses = 'block mb-2 text-xs font-bold tracking-wide uppercase';
+    let inputClasses = 'shadow border rounded w-full px-3 py-2 leading-tight text-gray-700';
+
+    if (invalid) {
+        labelClasses += ' text-red-600';
+        inputClasses += ' text-red-500 bg-red-100 border-red-300';
+    } else {
+        labelClasses += ' text-slate-700';
+        inputClasses += ' bg-stone-100 text-gray-700';
+    }
+
+    return (
+        <p>
+            <label className={labelClasses}>{label}</label>
+            <input className={inputClasses} {...props} />
+        </p>
+    );
+}
